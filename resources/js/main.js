f0c167252a20e28191f68c972dff34cadbf66d45
unknown <rafiirfan211@gmail.com>
2021-04-24 16:20:13 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function (){
+
+    $(".js--services-section").waypoint(function(direction){
+             if(direction == "down"){
+                 $("nav").addClass("sticky");
+             } else{
+                 $("nav").removeClass("sticky");
+             }
+
+    });
+
+    var mixer = mixitup('.container');
+});
